bfe15a7dfade08bac38fb539ffa17c15a3b187c4
acailly <ylliac14@gmail.com>
2018-04-13 17:32:20 +0200

    :hammer: Allow command with spaces without having to put quotes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const execSync = require("child_process").execSync;
 
 module.exports = function(vorpal) {
   vorpal
-    .command("exec <command>")
+    .command("exec [commands...]")
     .option("-p, --path", "Path the command is executed in")
     .description("Execute a command")
     .action(function(args, callback) {
@@ -11,7 +11,7 @@ module.exports = function(vorpal) {
         opts.cwd = args.options.path;
       }
       try {
-        const stdio = execSync(args.command, opts);
+        const stdio = execSync(args.commands.join(" "), opts);
         console.log(stdio.toString());
       } catch (e) {}
       callback();
